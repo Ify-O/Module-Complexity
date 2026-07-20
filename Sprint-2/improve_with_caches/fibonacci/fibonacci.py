de984fb041de828a2f352fb2d2d83770107b2a2b
Ifeoma <osegboifeoma3@gmail.com>
2026-07-20 09:59:08 +0200

    Optimize Fibonacci implementation

--- Sprint-2/improve_with_caches/fibonacci/fibonacci.py
+++ Sprint-2/improve_with_caches/fibonacci/fibonacci.py
@@ -1,4 +1,19 @@
 def fibonacci(n):
+    """
+    Return the nth Fibonacci number.
+
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    Optimal Time Complexity: O(n)
+    """
+
     if n <= 1:
         return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+
+    previous = 0
+    current = 1
+
+    for _ in range(2, n + 1):
+        previous, current = current, previous + current
+
+    return current
